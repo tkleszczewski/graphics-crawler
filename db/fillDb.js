637fe9413e806200a3b40eae72c77434531a6268
Tomasz Kleszczewski <tomekkleszczewski@gmail.com>
2021-02-25 12:45:16 +0100

    Add data updating if changed

--- db/fillDb.js
+++ db/fillDb.js
@@ -1,13 +1,29 @@
 const getGraphicCards = require('../crawler/getGraphicCards');
 const GraphicCard = require('./models/graphicCard.model');
+const { DateTime } = require('luxon');
 
 async function fillDb() {
+    const dt = DateTime.now();
     const graphicCards = await getGraphicCards();
-    const storedGraphicCards = await GraphicCard.find({});
-    console.log(storedGraphicCards);
+    const storedGraphicCards = await GraphicCard.find({ date: `${dt.day}/${dt.month}/${dt.year}`});
     if (storedGraphicCards.length === 0) {
-        await GraphicCard.insertMany(graphicCards);
+        return await GraphicCard.insertMany(graphicCards);
     }
+
+    const graphicCardsCheck = new Promise((resolve, reject) => {
+        graphicCards.forEach(async (graphicCard, index) => {
+            const { setup, producer, ecommerce, date, memory, price, isAvailable } = graphicCard;
+            const graphicCardFromDB = await GraphicCard.findOne({ setup, producer, ecommerce, date, memory, price, isAvailable });
+            if (!graphicCardFromDB) {
+                await GraphicCard.findOneAndUpdate({ setup, producer, ecommerce, date, memory }, { price, isAvailable }, { useFindAndModify: false });
+            }
+            if(index === (graphicCards.length - 1)) {
+                resolve();
+            }
+        });
+    });
+
+    await graphicCardsCheck;
 }
 
 module.exports = fillDb;
